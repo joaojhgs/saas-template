998c26d1e6c238aa8292655d98b87d31feeebfe3
joaojhgs <joaosperandio@alunos.utfpr.edu.br>
2024-04-28 17:58:57 -0300

    fix: add supabase libs and auth middleware integration with next-intl

---
+++ src/server/controller.ts
@@ -0,0 +1,10 @@
+'use server';
+
+/*
+    This is a controller file. It is used to define the functions that will be used by the server.
+    The functions defined here will be called by the server to perform the necessary operations.
+*/
+
+export const getAgendamento = async (id: string) => {
+  console.log('getAgendamento', id);
+};
---
+++ src/utils/supabase/server.ts
@@ -0,0 +1,37 @@
+import { cookies } from 'next/headers';
+
+import { type CookieOptions, createServerClient } from '@supabase/ssr';
+
+import { env } from '@/env';
+
+export function createClient() {
+  const cookieStore = cookies();
+
+  // Create a server's supabase client with newly configured cookie,
+  // which could be used to maintain user's session
+  return createServerClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY, {
+    cookies: {
+      get(name: string) {
+        return cookieStore.get(name)?.value;
+      },
+      set(name: string, value: string, options: CookieOptions) {
+        try {
+          cookieStore.set({ name, value, ...options });
+        } catch (error) {
+          // The `set` method was called from a Server Component.
+          // This can be ignored if you have middleware refreshing
+          // user sessions.
+        }
+      },
+      remove(name: string, options: CookieOptions) {
+        try {
+          cookieStore.set({ name, value: '', ...options });
+        } catch (error) {
+          // The `delete` method was called from a Server Component.
+          // This can be ignored if you have middleware refreshing
+          // user sessions.
+        }
+      },
+    },
+  });
+}
