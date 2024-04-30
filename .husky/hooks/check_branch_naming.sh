7a07a0b931d6533e15f541dbf5adb5123238773e
joaojhgs <joaosperandio@alunos.utfpr.edu.br>
2024-04-30 18:27:17 -0300

    feat: add file naming convetions and fix branch naming convention

--- .husky/hooks/check_branch_naming.sh
+++ .husky/hooks/check_branch_naming.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 local_branch_name="$(git rev-parse --abbrev-ref HEAD)"
 
-valid_branch_regex='^((hotfix|bugfix|fix|feature|improvement|chore|style|refactor)\/[a-zA-Z0-9_\-]+)$'
+valid_branch_regex='^((hotfix|bugfix|fix|feature|improvement|chore|style|refactor)\/[a-zA-Z0-9_\-]+)|(main)$'
 
 message="There is something wrong with your branch name. Branch names in this project must adhere to this contract: $valid_branch_regex. Your commit will be rejected. You should rename your branch to a valid name and try again."
 
---
+++ .husky/hooks/check_files_naming.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Function to check if a string is in kebab-case
+is_kebab_case() {
+    local string="$1"
+    string="${string%.*}"  # Remove file extension
+    string="${string%.*}"  # Remove double file extension
+    string="${string//[\[\]]/}"  # Remove special characters []
+    if [[ "$string" =~ ^[a-z0-9]+(-[a-z0-9]+)*$ || "$string" =~ ^[a-z0-9]+$ ]]; then
+        return 0
+    else
+        echo "$string"
+        return 1
+    fi
+}
+
+# Function to check if a string is in SnakeCase
+is_snake_case() {
+    local string="$1"
+    if [[ "$string" =~ ^[A-Z][a-zA-Z0-9]*$ ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+error=0
+# Check all files and folders inside the src folder
+for item in $(find src -type d -o -type f); do
+    # Exclude .git, .next, and node_modules directories
+    if [[ "$item" != ".git"* && "$item" != ".next"* && "$item" != "node_modules"* && "$item" != "src/locale/messages"* ]]; then
+        # Check if it's a folder
+        if [[ -d "$item" ]]; then
+            # Check kebab-case naming for folders
+            if ! is_kebab_case "$(basename "$item")"; then
+                echo "Error: Folder '$item' does not follow kebab-case naming convention."
+                error=1
+            fi
+        else
+            # Check SnakeCase naming for .tsx files inside components folder
+            if [[ "$item" == "src/components/"*".tsx" ]]; then
+                if ! is_snake_case "$(basename "$item" .tsx)"; then
+                    echo "Error: File '$item' does not follow SnakeCase naming convention."
+                    error=1
+                fi
+            else
+                # Check kebab-case naming for other files
+                if ! is_kebab_case "$(basename "$item")"; then
+                    echo "Error: File '$item' does not follow kebab-case naming convention."
+                    error=1
+                fi
+            fi
+        fi
+    fi
+done
+
+exit $error
