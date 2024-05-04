ad47857e06d69363953a0804a7a6372e3c24cde7
KozielGPC <gpcgabriel0@gmail.com>
2024-05-04 18:35:20 -0300

    feat: add latitude and longitude to barbershops

--- src/server/migrations/1-create-barber-and-barbershop-and-previous-services-tables.sql
+++ src/server/migrations/1-create-barber-and-barbershop-and-previous-services-tables.sql
@@ -12,7 +12,9 @@ CREATE TABLE barbershops (
     description VARCHAR(255),
     picture VARCHAR(255),
     document VARCHAR(255),
-    owner_id UUID
+    owner_id UUID,
+    latitude VARCHAR(255),
+    longitude VARCHAR(255)
 );
 
 CREATE TABLE previous_services (
