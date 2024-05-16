ab2465940baf1ae6b291ac4619c1c5b3de77f478
Márcio Gabriel <marcio.campos@luizalabs.com>
2024-05-15 22:23:46 -0300

    feat: add barbershop page

--- src/client/components/admin/barbershop/BarbershopComponent.tsx
+++ src/client/components/admin/barbershop/BarbershopComponent.tsx
@@ -1,77 +1,52 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import { Card, Flex, Typography } from 'antd';
+import { Col, Divider, Flex, Image, Layout, Row, Typography } from 'antd';
 
-const BarbershopComponent: React.FC = () => {
-  const [cardsPerRow, setCardsPerRow] = useState(5);
+const { Title, Paragraph } = Typography;
 
-  useEffect(() => {
-    function handleResize() {
-      const screenWidth = window.innerWidth;
-      if (screenWidth >= 1200) {
-        setCardsPerRow(4);
-      } else if (screenWidth >= 992) {
-        setCardsPerRow(3);
-      } else if (screenWidth >= 768) {
-        setCardsPerRow(2);
-      } else {
-        setCardsPerRow(1);
-      }
-    }
-
-    window.addEventListener('resize', handleResize);
-
-    handleResize();
-
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  return (
-    <Flex className="w-full" vertical={true}>
-      <Card className="p-10" title="Informações da Barbearia">
-        <Flex className="w-full p-10">
-          <img
-            alt="avatar"
-            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-            style={{ marginRight: '20px' }}
+const BarbershopPage = () => {
+  const renderBarberCard = () => {
+    return (
+      <Col span={4}>
+        <Flex vertical={true} className="items-center p-10">
+          <Image
+            src="https://t4.ftcdn.net/jpg/02/10/97/19/360_F_210971959_wXcBYfif7jKeyKkHKhVyOnzQWHawIgK4.jpg"
+            alt="barbershop"
           />
-          <Flex vertical={true} className="w-full p-10">
-            <Typography.Title level={1}>Nome da Barbearia</Typography.Title>
-            <Typography.Title level={3}>
-              Descrição da Barbearia Lorem ipsum dolor sit amet consectetur,
-              adipisicing elit. Quas in doloremque assumenda qui temporibus quae
-              non, aperiam aliquam quisquam iure error fugiat quidem ipsum ea
-              placeat deleniti perspiciatis obcaecati sequi!
-            </Typography.Title>
-          </Flex>
+          <Title level={4}>Nome barbearia</Title>
         </Flex>
-      </Card>
-
-      <Card className="p-10" title="Barbeiros">
-        <Flex wrap="wrap" className="w-full p-10">
-          {Array.from({ length: 8 }, (_, index) => (
-            <Card
-              key={index}
-              style={{
-                width: `${100 / cardsPerRow}%`,
-                margin: '0 10px 20px 0',
-              }}
-            >
-              <img
-                alt="avatar"
-                src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-              />
-              <Typography.Title level={4}>
-                Barbeiro {index + 1}
-              </Typography.Title>
-            </Card>
-          ))}
-        </Flex>
-      </Card>
-    </Flex>
+      </Col>
+    );
+  };
+  return (
+    <Layout>
+      <Row className="m-10 p-10">
+        <Col span={12}>
+          <Image
+            src="https://t4.ftcdn.net/jpg/02/10/97/19/360_F_210971959_wXcBYfif7jKeyKkHKhVyOnzQWHawIgK4.jpg"
+            alt="barbershop"
+          />
+        </Col>
+        <Col span={12}>
+          <Title>BarberShop</Title>
+          <Paragraph>
+            BarberShop description Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Provident, mollitia! Aperiam reiciendis quod
+            harum, id quia aut nisi quae unde eligendi cupiditate eius quam
+            adipisci atque consequuntur laudantium ex dolore.
+          </Paragraph>
+        </Col>
+      </Row>
+      <Divider />
+      <Row>
+        {[...Array(12)].map((_, index) => (
+          <React.Fragment key={index}>{renderBarberCard()}</React.Fragment>
+        ))}
+      </Row>
+    </Layout>
   );
 };
 
-export default BarbershopComponent;
+export default BarbershopPage;
