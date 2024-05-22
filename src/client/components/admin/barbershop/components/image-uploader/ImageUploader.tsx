f9c78c791e6c46a3161f2f8ac7c5636dc77348c4
joaojhgs <joaosperandio@alunos.utfpr.edu.br>
2024-05-21 21:34:22 -0300

    fix: use antd upload

--- src/client/components/admin/barbershop/components/image-uploader/ImageUploader.tsx
+++ src/client/components/admin/barbershop/components/image-uploader/ImageUploader.tsx
@@ -1,88 +1,29 @@
 'use client';
 
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 
-import { Button, Image, Spin } from 'antd';
-
-import {
-  useEditBarbershop,
-  useGetBarbershop,
-} from '@/client/hooks/useBarbershop';
+import { Upload, UploadFile, UploadProps } from 'antd';
+import ImgCrop from 'antd-img-crop';
 
 const ImageUploader = () => {
-  const { data } = useGetBarbershop();
-  const { mutate: editBarbershop } = useEditBarbershop();
-  const fileInputRef = useRef<HTMLInputElement>(null);
-  const [loading, setLoading] = useState(false);
-
-  const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    let file: File | null = null;
-
-    if (e.target.files) {
-      file = e.target.files[0];
-    }
-
-    if (file) {
-      setLoading(true);
-      const formData = new FormData();
-      formData.append('files', file);
-
-      const requestOptions = { method: 'POST', body: formData };
-
-      try {
-        const response = await fetch('/api/upload', requestOptions);
-        const result = await response.json();
-
-        const fullPath = result.data.fullPath;
-
-        editBarbershop({
-          id: data?.data?.id ?? '',
-          picture: fullPath,
-        });
-      } catch (error) {
-        console.error('Error uploading file:', error);
-      } finally {
-        setLoading(false);
-      }
-    }
-  };
-
-  const triggerFileInput = () => {
-    fileInputRef.current?.click();
+  // const { data } = useGetBarbershop();
+  // const { mutate: editBarbershop } = useEditBarbershop();
+  const [fileList, setFileList] = useState<UploadFile[]>([]);
+  const onChange: UploadProps['onChange'] = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
   };
 
   return (
-    <div className="relative inline-block">
-      {loading ? (
-        <div className="absolute inset-0 flex items-center justify-center">
-          <Spin />
-        </div>
-      ) : (
-        <Image
-          src={`https://nhtgmvgjkojhxrtjzzyv.supabase.co/storage/v1/object/public/${data?.data?.picture}`}
-          alt="barbershop"
-          className="max-h-full w-full object-cover"
-        />
-      )}
-      <Button
-        onClick={triggerFileInput}
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          display: loading ? 'none' : 'block',
-        }}
+    <ImgCrop rotationSlider>
+      <Upload
+        action="/api/upload"
+        listType="picture"
+        fileList={fileList}
+        onChange={onChange}
       >
-        Edit Image
-      </Button>
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleUpload}
-        style={{ display: 'none' }}
-        ref={fileInputRef}
-      />
-    </div>
+        {fileList.length < 5 && '+ Upload'}
+      </Upload>
+    </ImgCrop>
   );
 };
 
