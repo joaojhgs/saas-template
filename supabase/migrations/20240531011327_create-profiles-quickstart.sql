eb7c4f7e16a53207e6055f9172fa4839a599c11e
joaojhgs <joaosperandio@alunos.utfpr.edu.br>
2024-06-02 17:39:25 -0300

    fix: profile avatars bucket

--- supabase/migrations/20240531011327_create-profiles-quickstart.sql
+++ supabase/migrations/20240531011327_create-profiles-quickstart.sql
@@ -38,12 +38,12 @@ create trigger on_auth_user_created
 
 -- Set up Storage!
 insert into storage.buckets (id, name, public)
-  values ('provile-avatars', 'profile-avatars', true);
+  values ('profile_avatars', 'profile_avatars', true);
 
 -- Set up access controls for storage.
 -- See https://supabase.com/docs/guides/storage#policy-examples for more details.
 create policy "Avatar images are publicly accessible." on storage.objects
-  for select using (bucket_id = 'profile-avatars');
+  for select using (bucket_id = 'profile_avatars');
 
 create policy "Anyone can upload an avatar." on storage.objects
-  for insert with check (bucket_id = 'profile-avatars');
+  for insert with check (bucket_id = 'profile_avatars');
